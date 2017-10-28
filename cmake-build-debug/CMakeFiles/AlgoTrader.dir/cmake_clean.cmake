ec66a277dded3673eb8f1e8c4c62dfb54f38f11c
Chris <chris.bean91@gmail.com>
2017-10-28 12:52:06 +0100

    add CMake File

---
+++ cmake-build-debug/CMakeFiles/AlgoTrader.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AlgoTrader.dir/src/backtesters/benchmarks/BasicInvestor.cpp.o"
+  "CMakeFiles/AlgoTrader.dir/src/backtesters/BasicBackTester.cpp.o"
+  "CMakeFiles/AlgoTrader.dir/src/common/CSVRow.cpp.o"
+  "CMakeFiles/AlgoTrader.dir/src/DataLoaders/CSVDataLoader.cpp.o"
+  "CMakeFiles/AlgoTrader.dir/src/finance/exitconditions/NeverExitCondition.cpp.o"
+  "CMakeFiles/AlgoTrader.dir/src/finance/Portfolio.cpp.o"
+  "CMakeFiles/AlgoTrader.dir/src/finance/Position.cpp.o"
+  "CMakeFiles/AlgoTrader.dir/src/finance/Stock.cpp.o"
+  "CMakeFiles/AlgoTrader.dir/src/finance/Wallet.cpp.o"
+  "CMakeFiles/AlgoTrader.dir/src/tradeloops/MinuteLoop.cpp.o"
+  "CMakeFiles/AlgoTrader.dir/src/traders/BasicTrader.cpp.o"
+  "CMakeFiles/AlgoTrader.dir/src/AlgoTrader.cpp.o"
+  "AlgoTrader.pdb"
+  "AlgoTrader"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AlgoTrader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
